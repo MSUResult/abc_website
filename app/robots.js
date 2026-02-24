3f815be35175163457884e9c64b39c68b3bac0a3
MSUResult <adarshrajput9040@gmail.com>
2026-02-24 17:03:13 +0530

    first commit

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://abcinstitute.org/sitemap.x",
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://abcinstitute.org",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abcinstitute.org/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abcinstitute.org/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abcinstitute.org/courses",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abcinstitute.org/blog",
+      lastModified: new Date(),
+    },
+  ];
+}
