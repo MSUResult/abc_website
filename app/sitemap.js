df77f5b85e6f74c91bcdc04603897376b46ea317
MSUResult <shivanshsingh4539@gmail.com>
2026-05-06 15:17:40 +0530

    checking

--- app/sitemap.js
+++ app/sitemap.js
@@ -7,13 +7,19 @@ export default function sitemap() {
       priority: 1,
     },
     {
-      url: "https://abcinstitute.org/about",
+      url: "https://abcinstitute.org/about-us",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
     },
     {
       url: "https://abcinstitute.org/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+     {
+      url: "https://abcinstitute.org/store",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
@@ -26,6 +32,30 @@ export default function sitemap() {
     },
     {
       url: "https://abcinstitute.org/blog",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+     {
+      url: "https://abcinstitute.org/test-series",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+       {
+      url: "https://abcinstitute.org/neet/results-2026",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+     {
+      url: "https://abcinstitute.org/academic/results-2026",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+     {
+      url: "https://abcinstitute.org/jee/results-2026",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.9,
